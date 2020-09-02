c8fbcffd0eb4eb209ee54b4d9629f106e57c70d7
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-09-02 10:44:13 +0200

    initial commit

---
+++ Class2/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult JsonData()
+        {
+            Pizza pizza = new Pizza()
+            {
+                Id = 1,
+                Name = "Capri"
+            };
+
+            return new JsonResult(pizza);
+        }
+        public IActionResult BackToHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Details(int? Id)
+        {
+            if(Id != null)
+            {
+                return View();
+            }
+            return new EmptyResult();
+        }
+    }
+}
