43cde872325c211f873766e57aa5319b3f5521b2
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-09-02 13:40:18 +0200

    class2 homework

---
+++ SEDC.PizzaApp/Controllers/OrdersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new EmptyResult();
+            }
+            var Orders = OrdersList.Orders;
+            return View(Orders);
+        }
+        public IActionResult JsonData()
+        {
+            Employee Jason = new Employee
+            {
+                Id = 1,
+                Name = "Jason Statham"
+            };
+            return new JsonResult(Jason);
+        }
+        public IActionResult ReturnHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SEDC.PizzaApp/OrdersList.cs
@@ -0,0 +1,35 @@
+﻿using SEDC.PizzaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp
+{
+    public class OrdersList
+    {
+        public static List<Order> Orders = new List<Order>
+        {
+            new Order()
+            {
+                Id = 1,
+                Description = "Capricioza"
+            },
+            new Order()
+            {
+                Id = 2,
+                Description = "Pasto"
+            },
+            new Order()
+            {
+                Id = 3,
+                Description = "SpicyPizza"
+            },
+            new Order()
+            {
+                Id = 4,
+                Description = "Family sized Capricioza"
+            }
+        };
+    }
+}
