2a01582630f100d3e1fbd4a0924ce56e61678e9f
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-09-07 11:05:36 +0200

    Class4

---
+++ Class4/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.Mappers;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        List<PizzaViewModel> pizzas = new List<PizzaViewModel>();
+        public IActionResult Index()
+        {
+            ViewData.Add("Title", "Index");
+            foreach (Pizza pizza in StaticDb.Pizzas)
+            {
+                pizzas.Add(PizzaViewModelMapper.PizzaViewModel(pizza));
+            }
+            return View(pizzas); // returns ViewResult
+        }
+
+        public IActionResult JsonData()
+        {
+            Pizza pizza = new Pizza
+            {
+                Id = 1,
+                Name = "Capri"
+            };
+            return new JsonResult(pizza); // returns JsonResult
+        }
+
+        public IActionResult BackToHome()
+        {
+            return RedirectToAction("Index", "Home"); //redirects to Action Index in Home Controller
+        }
+
+        public IActionResult Details(int? id) // localhost:port/Pizza/Details/1 or  localhost:port/Pizza/Details
+        {
+            ViewData.Add("Title", "Details");
+            if (id != null)
+            {
+                var pizza = PizzaViewModelMapper.PizzaViewModel(StaticDb.Pizzas.SingleOrDefault(x => x.Id == id));
+                ViewBag.Name = pizza.Name;
+                ViewBag.Price = pizza.Price;
+                return View(pizza);
+            }
+            return new EmptyResult();
+        }
+    }
+}
---
+++ Class4/SEDC.PizzaApp/SEDC.PizzaApp/Models/Mappers/PizzaViewModelMapper.cs
@@ -0,0 +1,33 @@
+﻿using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.Mappers
+{
+    public static class PizzaViewModelMapper
+    {
+        public static PizzaViewModel PizzaViewModel(Pizza pizza)
+        {
+            decimal newPrice = 0;
+            if (pizza.HasExtras)
+            {
+                newPrice = pizza.Price + 10;
+            }
+            else
+            {
+                newPrice = pizza.Price;
+            }
+            return new PizzaViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                Price = newPrice,
+                PizzaSize = pizza.PizzaSize,
+                IsOnPromotion = pizza.IsOnPromotion
+            };
+        }
+    }
+}
